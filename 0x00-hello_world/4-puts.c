7db925afe9cb90bccce749f9ae887c04a6e004a6
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-10 06:50:35 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts ("\"Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/*
+ *
+ *script for using puts function to print a string
+ *
+ */
+
+/*main function where we write our code we need to run*/
+int main()
+{
+	puts ("\"Programming is like building a multilingual puzzle"); //like printf prints string on the screen
+	return 0;
+}
