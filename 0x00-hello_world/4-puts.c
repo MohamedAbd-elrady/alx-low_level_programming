8c48aa7393cc57cfb4bdc6a3053dfe06039f06e3
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-10 17:47:25 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 /*
  *
  *main - Entry point
- *
  *Return: Always 0 (Success)
  *
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /*like printf prints string on the screen*/
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+} 
