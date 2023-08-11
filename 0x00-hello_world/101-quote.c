fc9ed0eff1792a96f1a54d2edee4c9738585eebf
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-10 18:24:06 -0700

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  *main - Entry point
- *return : always 1
+ *Return: always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
