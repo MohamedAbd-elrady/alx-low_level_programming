2cbd47b307ed5c91942bb3b4ff521b308a960709
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-11 18:11:28 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' || c != 'o')
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
