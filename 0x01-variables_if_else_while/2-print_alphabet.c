5e98fe254f0197408c037543631075acf333fa91
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-11 18:02:29 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
