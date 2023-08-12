a7c56d5ae7254ac689f3df8026f0259c9ce2ebd5
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-11 18:22:46 -0700

    My first commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,10 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i=0; i < 10; i++)
+	for (int i=0; i < 10; i++)
 	{
-		printf("%d");
+		printf("%d", i);
 	}
-	printf("\n", i);
+	printf("\n");
 	return (0);
 }
