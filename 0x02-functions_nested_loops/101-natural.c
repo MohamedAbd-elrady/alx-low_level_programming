77d85c935472ae0b14aa23b3fb1bdf00e64a95f0
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 14:01:02 -0700

    My first commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -18,6 +18,7 @@ int main(void)
 		{
 			res += i;
 		}
+		i++;
 	}
 	printf("%d", res);
 	return (0);
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, x, y, z;
+
+	x = 0;
+	y = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+
+		printf("%d", z);
+
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
