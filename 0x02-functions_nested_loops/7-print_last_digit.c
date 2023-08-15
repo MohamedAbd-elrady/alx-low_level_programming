f0dcf78c67465feb6cc9048a6aa1ed7f05e0f90e
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 10:30:10 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: number to check
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
