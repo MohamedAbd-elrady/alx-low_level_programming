ca51c57f99ec18fe2df54db738b3c17b32721b16
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 08:10:12 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *print_alphabet - prints lowercase alphabet
+ *
+ *Return: 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+}
