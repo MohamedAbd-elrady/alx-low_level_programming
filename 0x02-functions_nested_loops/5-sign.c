da01a9be9761b1242d56268edc7a093bc30e1c9f
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 08:36:10 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ *
+ * 
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
