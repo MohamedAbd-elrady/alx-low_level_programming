94a0c9a05cabe6d50bb0d9e18f33ddf01c5ce136
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 12:47:15 -0700

    My first commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,64 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - print natural numbers untill 98
- * @n: passed number
+ * print_to_98 - print natural number until it reaches 98
+ * @n: number passed
  */
 
 void print_to_98(int n)
 {
-	if (n != 98)
+	while (n > 98)
 	{
-		while (n > 98)
-		{
-			if (n >= 100)
-			{
-				_putchar('0' + (n / 100));
-				_putchar('0' + ((n / 10)) % 10);
-			}
-			else
-			{
-				_putchar('0' + (n / 10));
-			}
-			_putchar('0' + (n % 10));
-			_putchar(',');
-			_putchar(' ');
-			n--;
-		}
-		while (n < 98)
-		{
-			if (n >= 0)
-			{
-				if (n >= 10)
-				{
-					_putchar('0' + (n / 10));
-				}
-				_putchar('0' + (n % 10));
-				_putchar(',');
-				_putchar(' ');
-				n++;
-			}
-			if (n < 0)
-			{
-				int nw;
-
-				nw = n * -1;
-
-				if (nw <= -10)
-				{
-					_putchar('0' + (nw / 10));
-				}
-				_putchar('0' + (nw % 10));
-				_putchar(',');
-				_putchar(' ');
-				n++;
-			}
-		}
+		printf("%d, ", n);
+		n--;
 	}
-	if (n == 98)
+	while (n < 98)
 	{
-		_putchar('0' + (n / 10));
-		_putchar('0' + (n % 10));
+		printf("%d, ", n);
+		n++;
 	}
-	_putchar('\n');
+	printf("%d\n", n);
 }
