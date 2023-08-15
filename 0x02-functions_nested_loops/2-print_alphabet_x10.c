d659c1bfd50f5f8dfaa3eed02a5d4dd19c4097b4
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 08:18:51 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints lowercase alphabet 10 times
+ *Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	for (char i = 0; i < 10; i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
