4b5220af297a5369baa0546401b7ef43ba2793fd
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 13:18:06 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - print tiimes table
+ * @n: passed number
+ */
+
+void print_times_table(int n)
+{
+	if (n < 15 || n > 0)
+	{
+		int row, coloum, res;
+
+		for (coloum = 0 ; coloum <= n; coloum++)
+		{
+			for (row = 0; row <= n; row++)
+			{
+				res = coloum * row;
+
+				if (res != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (res % 10));
+				}
+				else if (res >=10 && res <100)
+				{
+					_putchar(' ');
+					_putchar('0' + (res / 10));
+					_putchar('0' + (res % 10));
+				}
+				else if (res >= 100)
+				{
+					_putchar('0' + (res / 100));
+					_putchar('0' + (res / 10) % 10);
+					_putchar('0' + (res % 10));
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
