ac5ce52904c76524844289d4b78a93bde61404f3
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 14:09:32 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i, x, y, z, sum;
+
+	x = 0;
+	y = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+
+		if (z % 2 == 0 && z < 4000000)
+		{
+			sum += z;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
