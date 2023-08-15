1a8f038e6c64e068032224fbb90a89830a452912
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-15 11:46:09 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers untill 98
+ * @n: passed number
+ */
+
+void print_to_98(int n)
+{
+	if (n != 98)
+	{
+		while (n > 98)
+		{
+			_putchar('0' + (n / 10));
+			_putchar('0' + (n % 10));
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+		while (n < 98)
+		{
+			if (n > 10 && n < -10)
+			{
+				_putchar('0' + (n / 10));
+			}
+			_putchar('0' + (n % 10));
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+	else
+	{
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+}
