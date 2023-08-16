739fefaf99599c746ec63eb1660ddd121c435798
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-16 06:51:07 -0700

    My first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /*i not being incremented, so infinite loop happens*/
+	/*while (i < 10) 
 	{
 		putchar(i);
-	}
+	}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
