13a84d6a4fbd69896c1d1add7855e49411bd93aa
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-17 11:49:20 -0700

    My first commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,25 +7,32 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (n != 0)
 	{
-		_putchar('-');
-		n *= -1;
+		if (n < 0)
+		{
+			_putchar('-');
+			n *= -1;
+		}
+		if (n >= 1000)
+		{
+			_putchar('0' + (n / 1000));
+			_putchar('0' + (n / 100) % 10);
+			_putchar('0' + ((n % 100) / 10));
+		}
+		else if (n >= 100)
+		{
+			_putchar('0' + (n / 100));
+			_putchar('0' + ((n % 100) / 10));
+		}
+		else if (n >= 10)
+		{
+			_putchar('0' + (n / 10));
+		}
+		_putchar('0' + (n % 10));
 	}
-	if (n >= 1000)
+	else
 	{
-		_putchar('0' + (n / 1000));
-		_putchar('0' + (n / 100) % 10);
-		_putchar('0' + ((n % 100) / 10));
+		_putchar('0')
 	}
-	else if (n >= 100)
-	{
-		_putchar('0' + (n / 100));
-		_putchar('0' + ((n % 100) / 10));
-	}
-	else if (n >= 10)
-	{
-		_putchar('0' + (n / 10));
-	}
-	_putchar('0' + (n % 10));
 }
