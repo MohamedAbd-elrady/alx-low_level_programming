8b188e58048074256ffd9d6317f61c967a826401
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-17 09:55:44 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints the left half of the triangle
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, k, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (k = (size - i); k > 0; k--)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <=i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
