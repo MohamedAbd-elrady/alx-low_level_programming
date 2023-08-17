718977c6c4570d9c2b2c4ecf50e1b6efe7b3a4ca
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-17 07:48:54 -0700

    My first commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,16 @@
 #define _MAIN_H_
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
  * _isupper - checks for uppercase character
  * @c: character to check
  * Return: 1 if uppercase, 0 otherwise
