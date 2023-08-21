b6e1815e6d25f0d18afb49d469e3f8427812b1f9
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-21 05:36:46 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value to 98 with address
+ * @*n: pointer to the variable
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * reset_to_98 - reset the value to 98 with address
+ * @*n: pointer to the variable
+ */
+
+void reset_to_98(int *n);
+
+
+#endif
