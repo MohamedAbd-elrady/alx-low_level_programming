003874559fcbc65b38bf0b770e4cfe2ce0ada21c
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-08-21 12:07:26 -0700

    My first commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,16 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	/*while (*str != '\0')
 	{
 		_putchar(*str);
 		str += 2;
+	}*/
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
