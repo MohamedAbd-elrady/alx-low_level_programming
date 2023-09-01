c0c4d83f125c0c11431bf1962405521912732faf
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-01 14:34:33 -0700

    My first commit

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int matches = 0;
+
+	while (*(s + i))
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				matches++;
+				break;
+			}
+			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
+				return (matches);
+		}
+		i++;
+	}
+	return (matches);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value
+ * @n: number to check
+ * Return: the corrected value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = 0 - n;
+	}
+	return (n);
+}
