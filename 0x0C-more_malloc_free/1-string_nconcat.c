ecc1d6e4175be6ebef0952375f6c629030837f75
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-07 13:28:03 -0700

    My first commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -53,11 +53,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			ptr[i] = s1[i];
 		}
-		for (j = 1; j <= num; j++)
+		for (j = 0; j < num; j++)
 		{
 			ptr[i + j] = s2[j];
 		}
-			ptr[i + j + 1] = '\0';
+			ptr[i + j] = '\0';
 		return (ptr);
 	}
 }
