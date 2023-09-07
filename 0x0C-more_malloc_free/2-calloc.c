9d7e763f8d07759bb3875996cc6e475c1e8ae765
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-07 14:16:37 -0700

    My first commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *ptr;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
-		*(ptr + i) = 0;
+		ptr[i] = 0;
 
 	return (ptr);
 }
