b6d555f0fe4cd9041db4c661d01c5684f917af06
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-11 12:15:43 -0700

    My first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct that defines dog info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
