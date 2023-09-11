bf6e325f64584b413fe36083d5bbc95d2ed34578
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-11 08:28:06 -0700

    My first commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file the program was compiled from
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
