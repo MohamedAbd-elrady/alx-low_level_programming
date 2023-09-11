a1287b44cdd52a3538e7abea68c4f332a8b84dc9
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-11 13:12:40 -0700

    My first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,7 +23,7 @@ struct dog
  * @owner: owner name
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
 
 
 #endif
