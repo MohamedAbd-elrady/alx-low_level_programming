082bc7780850da5cbfe1efbf011e3fc2d377b5a6
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-13 16:37:09 -0700

    My first commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (ops[i].op == s)
+		if (s[0] == ops[i].op[0])
 		{
 			return (ops[i].f);
 		}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(argv[2]) == NULL || argv[3] != NULL)
+	if ((get_op_func(argv[2]) == NULL) || (s[1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
