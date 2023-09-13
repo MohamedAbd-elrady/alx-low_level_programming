41298c830e43d5070a9a79f4b9302e67fc6311c3
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-13 13:27:32 -0700

    My first commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,22 +1,7 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
- */
-
 int _putchar(char c);
-
-/**
- * print_name - function that prints a name.
- * @name: string to print
- * @f: pointer to function
- */
-
 void print_name(char *name, void (*f)(char *));
 
 #endif
