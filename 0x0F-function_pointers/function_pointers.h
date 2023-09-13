5226f14e197c8a45f2c9f9664275a523184071fe
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-13 13:37:54 -0700

    My first commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
-#include <stdlib.h>
+#define (void *)(0) NULL
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
