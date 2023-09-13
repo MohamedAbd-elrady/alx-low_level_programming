72deb241d7aaec546b04b3765afda6ba20af504b
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-13 13:09:55 -0700

    My first commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-
+#include "function_pointers.h"
 
 /**
  * print_name - function that prints a name.
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,3 @@
-function_pointers.h
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
