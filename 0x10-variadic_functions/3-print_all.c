b0c796d76c0876198581628bd70fb8dc01698562
MohammedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-14 16:17:34 -0700

    My first commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,95 @@
+#include "variadic_functions.h"
+
+/**
+ * pc - prints char
+ * @args: args
+ */
+
+void pc(va_list args)
+{
+	printf("%c", va_arg(args, char));
+}
+
+/**
+ * pi - prints int
+ * @args: args
+ */
+
+void pi(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+
+/**
+ * pf - prints float
+ * @args: args
+ */
+
+void pf(va_list args)
+{
+	printf("%f", va_arg(args, float));
+}
+
+/**
+ * ps - prints string
+ * @args: args
+ */
+
+void ps(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
+}
+
+/**
+ * print_all - function that prints anything.
+ * @format: a list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *separator;
+
+	da_typ p_form[] = {
+		{"c", pc},
+		{"i", pi},
+		{"f", pf},
+		{"s", ps}
+	};
+
+	int i, j;
+
+	va_start(args, format);
+
+	i = 0;
+	separator = "";
+
+	while (format != NULL && format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == p_form[j].letter)
+			{
+				printf("%s", separator);
+				format[j].func(args);
+				separator = ", ";
+			}
+			j++;
+		}
+	}
+
+	va_end(args);
+	printf("\n");
+
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,27 @@
 #define _VARIADIC_FUNCTIONS_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * struct types - struct that holds data types
+ * @letter: letter to check
+ * @func: pointer to function
+ */
+
+struct types
+{
+	char letter;
+	void (*func)(args);
+};
+
+typedef struct types da_typ;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
