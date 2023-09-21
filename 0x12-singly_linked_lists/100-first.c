417dfc427cb6ff59e5f8cd57b61d9f5ad5333457
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-21 08:08:33 -0700

    My first commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,7 @@
 /**
  * first - prints from init by the loader before main
  */
-
 void first(void) __attribute__ ((constructor));
-
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
