5cac4e27e275df14409ea84b178d297372d2383b
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-21 08:07:35 -0700

    My first commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * first - prints from init by the loader before main
+ */
+
+void first(void) __attribute__ ((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
