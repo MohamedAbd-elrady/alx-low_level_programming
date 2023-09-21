6cdcee640a91322f3f7b776cca58e68ad3446c15
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-21 07:45:08 -0700

    My first commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,12 @@
 size_t print_list(const list_t *h)
 {
 	size_t i;
-	int len;
 
 	i = 0;
 
 	while (h != NULL)
 	{
-		len = h->len;
-
-		if (h->str == NULL)
-			len = 0;
-		printf("[%d] %s\n", len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
