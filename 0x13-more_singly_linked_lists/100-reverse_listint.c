dee298551f8adf5adeeafeff4dcbb8ef0047b1fc
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-25 10:37:03 -0700

    My first commit

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,40 +1,29 @@
 #include "lists.h"
 
 /**
- * reverse_listint - Reverse a `listint_t` linked list
- * @head: double pointer to head
- * Description: Limited to going through loop only once.
- * Not allowed to use `malloc` or `free` or arrays
- * Can only declare a max of 2 variables.
+ * reverse_listint - reverse linked list
+ * @head: pointer to head pointer of linked list
  * Return: pointer to first node of reversed list
  */
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *hold;
-	listint_t *current;
+	listint_t *prev, *next;
 
-	if (*head == NULL)
+	/* account for no ptr and empty list */
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	current = *head;
-	*head = current->next;
-	hold = (*head)->next;
-	current->next = NULL;
-	if (*head == NULL)
+	/* iterate thorugh list to reverse linking */
+	prev = NULL;
+	while (*head != NULL)
 	{
-		*head = current;
-		return (current);
+		next = (*head)->next; /* keep track of next node to move to */
+		(*head)->next = prev; /* link current node's ptr to prev node */
+		prev = *head; /* update previous node to be current node */
+		*head = next; /* move current node to next node */
 	}
+	(*head) = prev; /* unlink last element from null and point backwards */
 
-	while (hold != NULL)
-	{
-		(*head)->next = current;
-		current = *head;
-		*head = hold;
-		hold = (*head)->next;
-	}
-
-	(*head)->next = current;
 	return (*head);
 }
