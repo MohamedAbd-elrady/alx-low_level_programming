cace99dee27f590954967a7028edc10b2947224c
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-09-28 07:24:09 -0700

    My first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,31 +8,31 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int StrToNum = 0, b_to_uint = 0, i = 0, twos;
+	unsigned int /*StrToNum = 0*/, b_to_uint = 0, i = 0, twos;
 
 	if (b == NULL)
 		return (0);
-
-	while (b[i] != '\0')
+	/**
+	*while (b[i] != '\0')
+	*{
+	*	if (b[i] == '0')
+	*		;
+	*	else if (b[i] == '1')
+	*		StrToNum += 1;
+	*	else
+	*		return (0);
+	*
+	*	if (b[i + 1] != '\0')
+	*		StrToNum *= 10;
+	*	i++;
+	*}
+	*/
+	for (twos = 1, i = 0; b[i] != '\0'; i++, twos *= 2)
 	{
-		if (b[i] == '0')
-			;
-		else if (b[i] == '1')
-			StrToNum += 1;
-		else
+		if (b[i] == '1')
+			b_to_uint += twos;
+		else if (b[i] != '0')
 			return (0);
-
-		if (b[i + 1] != '\0')
-			StrToNum *= 10;
-		i++;
-	}
-
-	if (StrToNum == 0)
-		return (0);
-
-	for (twos = 1; StrToNum != 0; StrToNum /= 10, twos *= 2)
-	{
-		b_to_uint += ((StrToNum % 10) * twos);
 	}
 
 	return (b_to_uint);
