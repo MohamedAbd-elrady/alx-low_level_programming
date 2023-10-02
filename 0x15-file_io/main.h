567d6082074e7835beca4421e1994c99c48334cb
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-10-02 13:37:33 -0700

    My first commit

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
